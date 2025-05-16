119b1680213d906b76ab7265aeaf0ea5ed991573
rescolies <58514612+rescolies@users.noreply.github.com>
2025-05-16 19:03:17 +0200

    Config inicial server

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  base: '/actum/' // 👈 muy importante para GitHub Pages
+  build: {
+    outDir: 'dist'
+  }
 })
