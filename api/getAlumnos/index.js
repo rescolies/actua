31a00612e2144f4a9ac28900e58aad4c0ba9ac0c
rescolies <58514612+rescolies@users.noreply.github.com>
2025-05-17 14:32:20 +0200

    Debug getAlumnos

--- api/getAlumnos/index.js
+++ api/getAlumnos/index.js
@@ -1,20 +1,23 @@
-const { CosmosClient } = require('@azure/cosmos')
+const { CosmosClient } = require('@azure/cosmos');
 
 module.exports = async function (context) {
-  const endpoint = process.env.COSMOS_DB_CONNECTION
-  const client   = new CosmosClient(endpoint)
-  const db       = client.database(process.env.COSMOS_DB_DATABASE)
-  const container = db.container(process.env.COSMOS_DB_CONTAINER)
+  try {
+    const client = new CosmosClient(process.env.COSMOS_DB_CONNECTION);
+    const container = client
+      .database(process.env.COSMOS_DB_DATABASE)
+      .container(process.env.COSMOS_DB_CONTAINER);
 
-  const { resources } = await container.items.readAll().fetchAll()
-  // Sólo devolvemos el nombre y la fecha de registro
-  const respuesta = resources.map(a => ({
-    nombre: a.nombre,
-    fechaRegistro: a.date
-  }))
+    const { resources } = await container.items
+      .query("SELECT * FROM c")
+      .fetchAll();
 
-  context.res = {
-    status: 200,
-    body: respuesta
+    context.res = { status: 200, body: resources };
+  } catch (err) {
+    // Aquí exponemos el error para leerlo desde el navegador
+    context.log.error('getAlumnos error:', err);
+    context.res = {
+      status: 500,
+      body: { error: err.message }
+    };
   }
-}
+};
